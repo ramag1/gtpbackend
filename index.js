e78aa9dd2cc1e72c061c9f2cdf84cdc38ac848ce
rmagley <rachelmagley@gmail.com>
2022-06-07 14:09:57 -0400

    Connect nodemailer

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const nodemailer= require('nodemailer')
-
 const inquiryController = require('./controllers/Inquiry');
-const { getMaxListeners } = require('./db/models/Inquiry');
+const nodemailer = require('nodemailer');
 
 // Middleware
 app.use(express.json());
@@ -17,43 +15,47 @@ app.get('/', (req, res) => {
 	res.redirect('/inquiries');
 });
 
-//NEW STUFF
-// app.get('/', (req, res) => {
-// 	res.sendFile('/inquiries');
-// });
+// nodemailer
+
+app.get('/', (req, res)=>{
+	res.sendFile('/inquires')
+})
 
 app.post('/inquiries', (req, res) => {
-	console.log(req.body)
+	console.log(req.body);
+
 	const transporter = nodemailer.createTransport({
 		service: 'outlook',
 		auth: {
-			user: 'rm1234562022@outlook.com',
-			pass: 'Ilovethis123',
+			user: process.env.AUTH_USER,
+			pass: process.env.AUTH_PASS,
 		},
 	});
 
 	const mailOptions = {
 		from: req.body.name,
 		to: 'rm1234562022@outlook.com',
-		subject: `Message from ${req.body.email}`,
-		text: `${req.body.name}, at ${req.body.email} has emailed you requesting the below information: ${req.body.comments}`
+		subject: `${req.body.name} at ${req.body.email} has submitted an inquiry.`,
+		text: req.body.comments,
 	};
 
-	transporter.sendMail(mailOptions, (error, info) => {
+	transporter.sendMail(mailOptions, (error, info)=> {
 		if(error){
 			console.log(error);
 			res.send('error');
-		} else {
-			console.log('Email sent: ' + info.response)
-			res.send('Success!')
+		}else {
+			console.log('email sent: ' + info.response);
+			res.send('success')
 		}
 	})
 })
+
+
 
 // Controllers
 // Forward all requests to localhost:3111/inquiries to the inquiry controller
 app.use('/inquiries', inquiryController);
 
 app.listen(3111, () => {
-	console.log('Inquiries are here on port 3111!');
+	console.log('Inquiries are here on port 3111');
 });
