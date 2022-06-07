60fc57f05895c45b554379ad0ab4fac6991ec143
rmagley <rachelmagley@gmail.com>
2022-06-07 13:38:04 -0400

    Connect nodemailer

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const nodemailer= require('nodemailer')
+
 const inquiryController = require('./controllers/Inquiry');
+const { getMaxListeners } = require('./db/models/Inquiry');
 
 // Middleware
 app.use(express.json());
@@ -14,10 +17,43 @@ app.get('/', (req, res) => {
 	res.redirect('/inquiries');
 });
 
+//NEW STUFF
+// app.get('/', (req, res) => {
+// 	res.sendFile('/inquiries');
+// });
+
+app.post('/inquiries', (req, res) => {
+	console.log(req.body)
+	const transporter = nodemailer.createTransport({
+		service: 'outlook',
+		auth: {
+			user: 'rm1234562022@outlook.com',
+			pass: 'Ilovethis123',
+		},
+	});
+
+	const mailOptions = {
+		from: req.body.name,
+		to: 'rm1234562022@outlook.com',
+		subject: `Message from ${req.body.email}`,
+		text: `${req.body.name}, at ${req.body.email} has emailed you requesting the below information: ${req.body.comments}`
+	};
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if(error){
+			console.log(error);
+			res.send('error');
+		} else {
+			console.log('Email sent: ' + info.response)
+			res.send('Success!')
+		}
+	})
+})
+
 // Controllers
 // Forward all requests to localhost:3111/inquiries to the inquiry controller
 app.use('/inquiries', inquiryController);
 
 app.listen(3111, () => {
-	console.log('Inquiries are here!');
+	console.log('Inquiries are here on port 3111!');
 });
